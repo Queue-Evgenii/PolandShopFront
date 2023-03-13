f774039eb336c49cbffdbfdeb3ba8ec637c3438e
Queue-Evgenii <genadolgii228@gmail.com>
2023-03-13 17:52:34 +0200

    eslint

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: '/shop',
+  chainWebpack: config => {
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0].push({
+          from: 'public/.htaccess',
+          to: ''
+        })
+        return args
+      })
+  }
+}
