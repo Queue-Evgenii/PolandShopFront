04f9a08c3574db70d60dc59ade10f25d632401ef
Queue-Evgenii <genadolgii228@gmail.com>
2024-06-22 11:29:47 +0300

    payment successed/declined notification

--- src/views/ConfirmedView.vue
+++ src/views/ConfirmedView.vue
@@ -2,8 +2,27 @@
   <div class="confirmed">
     <layout-default>
       <div class="confirmed__data">
-        <h2>Payment successful!</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio velit consectetur sunt, molestiae itaque dum?</p>
+        <template v-if="paymentStatus != 'error'">
+          <h2>Payment successful!</h2>
+          <p>
+            Thank you for your purchase!
+            Your payment has been processed successfully.
+            You will receive a confirmation email shortly with the details of your order.
+          </p>
+          <p>
+            Your Order ID is: {{ orderId }}.
+          </p>
+        </template>
+        <template v-else>
+          <h2>Payment declined!</h2>
+          <p>
+            We're sorry, but your payment could not be processed at this time.
+            Please check your payment details and try again. If the issue persists, contact your bank or payment provider for further assistance.
+          </p>
+          <p>
+            Your Order ID is: {{ orderId }}.
+          </p>
+        </template>
       </div>
     </layout-default>
   </div>
@@ -15,6 +34,14 @@ export default {
   name: "confirmed", 
   components: {
     LayoutDefault
+  },
+  computed: {
+    paymentStatus() {
+      return this.$route.query.status;
+    },
+    orderId() {
+      return this.$route.query.order_id;
+    }
   },
 }
 </script>
