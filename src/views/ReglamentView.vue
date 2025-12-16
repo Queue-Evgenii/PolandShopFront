bb248140614536753881390dc8247d8287ecd3ce
unknown <yevhenii.dolhyi@gmail.com>
2025-12-16 19:14:30 +0200

    UPDATE: reglament and advertisement text

--- src/views/ReglamentView.vue
+++ src/views/ReglamentView.vue
@@ -156,20 +156,20 @@
 
           <p>c. Kupujący w momencie odbioru przesyłki, zobowiązany jest do sprawdzenia jej pod kątem zgodności z zamówieniem. Potwierdzając odbiór, Kupujący uzyskuje prawo własności towaru oraz ponosi wszelkie ryzyko związane z jego posiadaniem i użytkowaniem.</p>
 
-          <p>d. Każda reklamacja musi być zgłoszona pisemnie do właściciela Sklepu internetowego na jego adres poczty tradycyjnej. Sklep zastrzega sobie prawo do ustosunkowania się do żądań klienta w terminie 7 dni roboczych od momentu zgłoszenia niezgodności towaru. Jeżeli będzie wymagana dodatkowa ekspertyza techniczna do oceny reklamowanego towaru, termin ten może zostać wydłużony do 14 dni. Jeżeli w ciągu 14 dni sklep nie ustosunkuje się do żądań Zamawiającego, oznacza to, iż uznał żądanie za uzasadnione.</p>
-
+          <p>d. Każda reklamacja musi być zgłoszona pisemnie do właściciela Sklepu internetowego na jego adres poczty tradycyjnej UL. Aleja Krakowska 121 02-180 Warszawa. Sklep zastrzega sobie prawo do ustosunkowania się do żądań klienta w terminie 7 dni roboczych od momentu zgłoszenia niezgodności towaru. Jeżeli będzie wymagana dodatkowa ekspertyza techniczna do oceny reklamowanego towaru, termin ten może zostać wydłużony do 14 dni. Jeżeli w ciągu 14 dni sklep nie ustosunkuje się do żądań Zamawiającego, oznacza to, iż uznał żądanie za uzasadnione.</p>
+          
           <h2>ZWROTY</h2>
 
-          <p>a. Zamawiający może odstąpić od umowy bez podania przyczyn, składając stosowne oświadczenie na piśmie w terminie 14 dni, liczonym od dnia wydania rzeczy. Do zachowania tego terminu wystarczy wysłanie oświadczenia przed jego upływem.</p>
-
-          <p>b. W przypadku podjęcia decyzji o odstąpieniu od umowy, Zamawiający powinien odesłać przesyłkę należycie zapakowaną w terminie 14 dni od jej otrzymania. Przesyłka taka jest realizowana na koszt Zamawiającego. Przesyłka powinna być dobrze zabezpieczona, tak, aby nie doszło do jej uszkodzenia podczas transportu. Za wszelkie uszkodzenia produktu, opakowania powstałe podczas transportu odpowiedzialność ponosi Zamawiający. Tym samym odmawiamy prawa do zwrotu towaru uszkodzonego (lub z uszkodzonym opakowaniem, uniemożliwiającym dalszą odsprzedaż towaru). Towar uszkodzony podczas transportu zostanie zwrócony do Zamawiającego na jego koszt.</p>
-
+          <p>a. Zamawiający może odstąpić od umowy bez podania przyczyn, składając stosowne oświadczenie na piśmie w terminie dziesięciu dni, liczonym od dnia wydania rzeczy. Do zachowania tego terminu wystarczy wysłanie oświadczenia przed jego upływem.</p>
+          
+          <p>b. W przypadku podjęcia decyzji o odstąpieniu od umowy, Zamawiający powinien odesłać przesyłkę pod adres UL Zagrodowa 3, 04-992 Warszawa  należycie zapakowaną w terminie 14 dni od jej otrzymania. Przesyłka taka jest realizowana na koszt Zamawiającego. Przesyłka powinna być dobrze zabezpieczona, tak, aby nie doszło do jej uszkodzenia podczas transportu. Za wszelkie uszkodzenia produktu, opakowania powstałe podczas transportu odpowiedzialność ponosi Zamawiający. Tym samym odmawiamy prawa do zwrotu towaru uszkodzonego (lub z uszkodzonym opakowaniem, uniemożliwiającym dalszą odsprzedaż towaru). Towar uszkodzony podczas transportu zostanie zwrócony do Zamawiającego na jego koszt.</p>
+          
           <p>c. Zwroty dopuszczalne są tylko w przypadku towaru nieużywanego (nienoszącego jakichkolwiek śladów używania) oraz oryginalnie opakowanego, jeżeli towar występował w opakowaniu. Towar nie może być zwrócony, jeśli Zamawiający zdjął opakowanie (np. folię ochronną). Oryginalne opakowanie producenta nie może być naruszone i/lub uszkodzone.</p>
-
+          
           <p>d. Do zwracanego towaru należy dołączyć oryginalny rachunek/fakturę.</p>
-
+          
           <p>e. Jeśli wszystkie ww. warunki zwrotu towaru zostaną spełnione, zwrot pieniędzy w wysokości ceny odesłanego towaru (zwrotowi nie podlegają koszty dostawy), nastąpi w ciągu 7 dni roboczych od dnia otrzymania przez sklep zwróconego towaru.</p>
-
+          
           <p> </p>
 
           <h2>PRZERWY TECHNICZNE</h2>
